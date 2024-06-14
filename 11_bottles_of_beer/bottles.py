e78c612f023ec9b693d4175f0418364bd7d81625
김백일 <cedar101@gmail.com>
2024-06-14 14:26:29 +0900

    add unit test / introduce type_docopt

---
+++ 11_bottles_of_beer/bottles.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+"""
+usage: bottles.py [-h] [-n NUMBER]
+
+Bottles of beer song
+
+options:
+  -h, --help                show this help message and exit
+  -n NUMBER, --num NUMBER   How many bottles [type: int] [default: 10] 
+"""
+
+from type_docopt import docopt, DocoptExit
+from box import Box
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+    try:
+        args = Box(docopt(__doc__))
+    except ValueError as e:
+        strerr = str(e)
+        if "invalid literal for int() with base 10" in strerr:
+            _0, _1, invalid_literal = strerr.rpartition(" ")
+            strerr = f"invalid int value: {invalid_literal}"
+        raise DocoptExit(f"error: {strerr}") from e
+
+    if args.num < 1:
+        raise DocoptExit(f'--num "{args.num}" must be greater than 0')
+
+    return args
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    print("\n\n".join(map(verse, range(args.num, 0, -1))))
+
+
+# --------------------------------------------------
+def verse(bottle):
+    """Sing a verse"""
+
+    next_bottle = bottle - 1
+    s1 = "" if bottle == 1 else "s"
+    s2 = "" if next_bottle == 1 else "s"
+    num_next = "No more" if next_bottle == 0 else next_bottle
+    return "\n".join(
+        [
+            f"{bottle} bottle{s1} of beer on the wall,",
+            f"{bottle} bottle{s1} of beer,",
+            f"Take one down, pass it around,",
+            f"{num_next} bottle{s2} of beer on the wall!",
+        ]
+    )
+
+
+# --------------------------------------------------
+if __name__ == "__main__":
+    main()
---
+++ 11_bottles_of_beer/tests/cli_test.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+"""tests for bottles.py"""
+
+import hashlib
+import random
+import string
+from pathlib import Path
+from subprocess import check_output, CalledProcessError, STDOUT
+
+import pytest
+
+prg = "./bottles.py"
+
+with open("sums.txt") as f:
+    sums = dict(x.rstrip().split("\t") for x in f)  # .read().splitlines())
+
+
+# --------------------------------------------------
+@pytest.fixture(name="bad_str")
+def random_string() -> str:
+    return "".join(random.choices(string.ascii_lowercase + string.digits, k=5))
+
+
+# --------------------------------------------------
+@pytest.fixture(name="bad_int")
+def random_negative_integer() -> int:
+    return random.randint(-10, 0)
+
+
+# --------------------------------------------------
+@pytest.fixture(name="bad_float")
+def random_float() -> float:
+    return round(random.uniform(0, 10), random.randint(1, 10))
+
+
+# --------------------------------------------------
+def test_exists():
+    """exists"""
+    assert Path(prg).is_file()
+
+
+# --------------------------------------------------
+@pytest.mark.parametrize("flag", ["-h", "--help"])
+def test_usage(flag):
+    """usage"""
+    out = check_output([prg, flag])
+    assert b"usage" in out.lower()
+
+
+# --------------------------------------------------
+def test_string(bad_str):
+    """str value"""
+    with pytest.raises(CalledProcessError) as excinfo:
+        check_output([prg, "-n", bad_str], text=True, stderr=STDOUT)
+    assert "usage" in excinfo.value.stdout.lower()
+    assert f"invalid int value: '{bad_str}'" in excinfo.value.stdout
+
+
+# --------------------------------------------------
+def test_bad_int(bad_int):
+    """Bad integer value"""
+    with pytest.raises(CalledProcessError) as excinfo:
+        check_output([prg, "-n", str(bad_int)], text=True, stderr=STDOUT)
+    assert "usage" in excinfo.value.stdout.lower()
+    assert f'--num "{bad_int}" must be greater than 0' in excinfo.value.stdout
+
+
+# --------------------------------------------------
+def test_float(bad_float):
+    """float value"""
+    with pytest.raises(CalledProcessError) as excinfo:
+        check_output([prg, "--num", str(bad_float)], text=True, stderr=STDOUT)
+    assert "usage" in excinfo.value.stdout.lower()
+    assert f"invalid int value: '{bad_float}'" in excinfo.value.stdout
+
+
+# --------------------------------------------------
+def test_one():
+    """One bottle of beer"""
+
+    expected = (
+        "1 bottle of beer on the wall,\n"
+        "1 bottle of beer,\n"
+        "Take one down, pass it around,\n"
+        "No more bottles of beer on the wall!\n"
+    )
+
+    out = check_output([prg, "--num", "1"], text=True)
+    assert out == expected
+
+
+# --------------------------------------------------
+def test_two():
+    """Two bottles of beer"""
+
+    expected = (
+        "2 bottles of beer on the wall,\n"
+        "2 bottles of beer,\n"
+        "Take one down, pass it around,\n"
+        "1 bottle of beer on the wall!\n\n"
+        "1 bottle of beer on the wall,\n"
+        "1 bottle of beer,\n"
+        "Take one down, pass it around,\n"
+        "No more bottles of beer on the wall!\n"
+    )
+
+    out = check_output([prg, "-n", "2"], text=True)
+    assert out == expected
+
+
+# --------------------------------------------------
+@pytest.mark.parametrize("n", random.choices(list(sums.keys()), k=10))
+def test_random(n):
+    """Random number"""
+    flag = random.choice(["-n", "--num"])
+    out = check_output([prg, flag, n], text=True)
+    # out += "\n"  # because the last newline is removed
+    # breakpoint()
+    assert hashlib.md5(out.encode("utf-8")).hexdigest() == sums[n]
