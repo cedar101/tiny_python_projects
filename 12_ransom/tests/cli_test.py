fdf209bb701f98a54596d6971369a75b77ff80b6
김백일 <cedar101@gmail.com>
2024-06-20 17:18:36 +0900

    parametrized test

--- 12_ransom/tests/cli_test.py
+++ 12_ransom/tests/cli_test.py
@@ -10,8 +10,11 @@ import pytest
 
 
 programs = [f"./{p}" for p in Path(".").glob("solution*.py")]
-fox = "../inputs/fox.txt"
-now = "../inputs/now.txt"
+
+fox_text = "The quick brown fox jumps over the lazy dog."
+now_text = "Now is the time for all good men to come to the aid of the party."
+fox_file = "../inputs/fox.txt"
+now_file = "../inputs/now.txt"
 
 
 # --------------------------------------------------
@@ -39,29 +42,23 @@ def test_usage(prg, flag):
 # --------------------------------------------------
 @pytest.mark.parametrize("prg", programs)
 @pytest.mark.parametrize(
-    "seed,expected",
+    "in_text,seed,expected",
     [
-        ("1", "thE QUICk BrOWn Fox jumpS OveR tHe LAzY dOg."),
-        ("3", "thE quICk BROwn Fox jUmPS OVEr the lAZY DOG."),
+        (fox_text, "1", "thE QUICk BrOWn Fox jumpS OveR tHe LAzY dOg."),
+        (fox_text, "3", "thE quICk BROwn Fox jUmPS OVEr the lAZY DOG."),
+        (
+            now_text,
+            "2",
+            "now iS the TIME fOR ALl good meN TO COMe To THE AID oF THE PArTY.",
+        ),
+        (
+            now_text,
+            "5",
+            "NOw is tHE Time FOr all good men To coME TO tHe AiD OF THe ParTy.",
+        ),
     ],
 )
-def test_text1(prg, seed_flag, seed, expected):
-    in_text = "The quick brown fox jumps over the lazy dog."
-    out = check_output([prg, seed_flag, seed, in_text], text=True)
-    assert out.strip() == expected
-
-
-# --------------------------------------------------
-@pytest.mark.parametrize("prg", programs)
-@pytest.mark.parametrize(
-    "seed,expected",
-    [
-        ("2", "now iS the TIME fOR ALl good meN TO COMe To THE AID oF THE PArTY."),
-        ("5", "NOw is tHE Time FOr all good men To coME TO tHe AiD OF THe ParTy."),
-    ],
-)
-def test_text2(prg, seed_flag, seed, expected):
-    in_text = "Now is the time for all good men to come to the aid of the party."
+def test_text(prg, seed_flag, in_text, seed, expected):
     out = check_output([prg, seed_flag, seed, in_text], text=True)
     assert out.strip() == expected
 
@@ -71,10 +68,18 @@ def test_text2(prg, seed_flag, seed, expected):
 @pytest.mark.parametrize(
     "filename,seed,expected",
     [
-        (fox, "1", "thE QUICk BrOWn Fox jumpS OveR tHe LAzY dOg."),
-        (fox, "3", "thE quICk BROwn Fox jUmPS OVEr the lAZY DOG."),
-        (now, "2", "now iS the TIME fOR ALl good meN TO COMe To THE AID oF THE PArTY."),
-        (now, "5", "NOw is tHE Time FOr all good men To coME TO tHe AiD OF THe ParTy."),
+        (fox_file, "1", "thE QUICk BrOWn Fox jumpS OveR tHe LAzY dOg."),
+        (fox_file, "3", "thE quICk BROwn Fox jUmPS OVEr the lAZY DOG."),
+        (
+            now_file,
+            "2",
+            "now iS the TIME fOR ALl good meN TO COMe To THE AID oF THE PArTY.",
+        ),
+        (
+            now_file,
+            "5",
+            "NOw is tHE Time FOr all good men To coME TO tHe AiD OF THe ParTy.",
+        ),
     ],
 )
 def test_file(prg, seed_flag, filename, seed, expected):
